3fec2dd80982ffcb3a651231a2592fe0cf803db1
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2022-01-10 10:16:49 +0000

    HelloWorld

---
+++ HelloWorld/HelloWorld/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by Bisneto on 10/01/2022.
+//
+
+import Foundation
+
+print("Copyright © 2022 Heitor. All rights reserved")
+print("Hello World!")
+
---
+++ HelloWorld/README.md
@@ -0,0 +1,7 @@
+# HelloWorld
+
+Simple Swift "Hello World!"
+
+#
+
+Copyright © 2022 Heitor Bisneto. All rights reserved.
